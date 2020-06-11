135c4ba79a56399a3c952dfe09fe95594b8bf4e0
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-06-11 02:09:27 -0700

    scheme-model completed

--- data/db-config.js
+++ data/db-config.js
@@ -3,3 +3,4 @@ const knex = require('knex');
 const config = require('../knexfile.js');
 
 module.exports = knex(config.development);
+
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,2 +1,55 @@
-const db = require("../data/")
+const db = require("../data/db-config.js")
 
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add, 
+    update,
+    remove
+}
+
+
+
+
+function find() {
+    return db("schemes")
+}
+
+function findById(id) {
+    return db("schemes")
+    .where({ id })
+    .first() 
+}
+
+function findSteps(id) {
+    return db("steps")
+    .join('schemes',
+        'schemes.id',
+        'steps.scheme_id'
+    )
+    .select('steps.id', 
+        'schemes.scheme_name',
+        'steps.step_number',
+        'steps.instructions',
+    )
+    .orderBy('steps.step_number')
+    .where('steps.scheme_id', id)
+}
+
+function add(schemeData) {
+    return db("schemes")
+        .insert(schemeData)
+}
+
+function update(change, id) {
+    return db("schemes")
+    .where({ id })
+    .update(change)
+}
+
+function remove(id){
+    return db("schemes")
+    .where({ id })
+    .del()
+}
